baf9eaeb3a3e36577ae6539be8ae1c8d28fb6bf4
tdingman422 <tracey_dingman@outlook.com>
2024-11-18 15:14:03 -0500

    edited model_loader

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources, customer, promo
+from . import orders, order_details, recipes, sandwiches, resources, customer, promo, payment_method, review
 
 from ..dependencies.database import engine
 
@@ -11,3 +11,5 @@ def index():
     resources.Base.metadata.create_all(engine)
     customer.Base.metadata.create_all(engine)
     promo.Base.metadata.create_all(engine)
+    payment_method.Base.metadata.create_all(engine)
+    review.Base.metadata.create_all(engine)
