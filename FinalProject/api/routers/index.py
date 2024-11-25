a885e35d82390b511312f90a380a8b776222d827
tdingman422 <tracey_dingman@outlook.com>
2024-11-25 15:21:52 -0500

    added review to index

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,7 +1,8 @@
-from . import orders, order_details, promo
+from . import orders, order_details, promo, review
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(promo.router)
+    app.include_router(review.router)
