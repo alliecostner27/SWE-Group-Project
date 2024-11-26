83aa4967b29e77efc8424184275ab22dc526cf5a
tdingman422 <tracey_dingman@outlook.com>
2024-11-26 13:15:50 -0500

    added missing payment_method id

--- FinalProject/api/schemas/payment_method.py
+++ FinalProject/api/schemas/payment_method.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Literal
 
 class PaymentMethodBase(BaseModel):
+    id: int
     card_num: str = Field(..., description="credit/debit card number")
     transaction_status: Literal['pending', 'approved', 'failed'] = Field(..., description="transaction status")
     transaction_type: Literal['debit', 'credit'] = Field(..., description="transaction type")
