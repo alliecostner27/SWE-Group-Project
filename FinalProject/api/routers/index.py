14ee22d5ad19f2af9cb9c5103ae5cd20291dfc9f
tdingman422 <tracey_dingman@outlook.com>
2024-11-26 12:37:36 -0500

    added customer CRUD

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, promo, review
+from . import orders, order_details, promo, review, customer
 
 
 def load_routes(app):
@@ -6,3 +6,4 @@ def load_routes(app):
     app.include_router(order_details.router)
     app.include_router(promo.router)
     app.include_router(review.router)
+    app.include_router(customer.router)
